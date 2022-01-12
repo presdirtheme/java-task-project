08dded07d7121390f5ea881b323c43068db95e64
presdirtheme <presdirtheme@gmail.com>
2022-01-12 22:11:23 +0700

    learn about data type and variable

--- src/javatest/main.java
+++ src/javatest/main.java
@@ -2,6 +2,19 @@ package javatest;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // primitive data type
+        int helloWorld = 5;
+        double num2 = 5.5;
+        boolean b = true;
+        char c = 'X';
+
+        // non-primitive data type
+        String a = "1b%";
+
+        System.out.println(helloWorld);
+        System.out.println(num2);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(a);
 }
 }
