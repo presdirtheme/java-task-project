ab8abd97c679abca8b4a953b0e543e8efe9d4612
presdirtheme <presdirtheme@gmail.com>
2022-01-14 21:28:33 +0700

    learn more about basic operators

--- src/javatest/main.java
+++ src/javatest/main.java
@@ -2,8 +2,9 @@ package javatest;
 
 public class main {
     public static void main(String[] args) {
+        // Variable and Data Types
         // primitive data type
-        int helloWorld = 5;
+        /**int helloWorld = 5;
         double num2 = 5.5;
         boolean b = true;
         char c = 'X';
@@ -16,5 +17,13 @@ public class main {
         System.out.println(b);
         System.out.println(c);
         System.out.println(a);
+        **/
+
+        // Basic Operators
+        int x = 5;
+        int y = 7;
+        int z = 10;
+        int sum = x+y+z;
+        System.out.println(sum);
 }
 }
