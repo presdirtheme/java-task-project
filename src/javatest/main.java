967ea33ae31aa5d937910c92b651680e603d176a
presdirtheme <presdirtheme@gmail.com>
2022-01-14 22:22:30 +0700

    explaining what the basic operators does

--- src/javatest/main.java
+++ src/javatest/main.java
@@ -23,17 +23,22 @@ public class main {
         // Basic Operators
         int x = 5;
         int y = 7;
-        double z = 10;
+        double z = 10; // bilangan koma
         int v = 15;
+        int u = 53;
         int sum = x + y;
-        double sum2 = z / y;
-        double sum3 = v / (double) y;
+        double sum2 = z / y; // hasil menjadi double jika salah satu data type nya double, penampung hasil
+                             // operasi wajib double
+        double sum3 = v / (double) y; // v / y = hasil integer, jika pakai typecasting (double), hasil menjadi double
+        int sum4 = u % x; // u / x = hasil -> x * hasil(tanpa koma) = hasil modulus
 
-        double d = Math.pow(y, x);
+        double d = Math.pow(y, x); // y pangkat x = hasil
 
         System.out.println(sum);
         System.out.println(sum2);
-        System.out.println(sum3);
         System.out.println(d);
+        System.out.println(sum3);
+        System.out.println(sum4);
+
     }
 }
